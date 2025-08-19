3bbf8722a366ee7aa8bdaed3435c7b556d2c15ad
krrge <krrgegod@gmail.com>
2025-08-19 10:31:43 +0900

    Delete Windows11 右クリの仕様変更 directory

--- Windows11 右クリの仕様変更/RightClick New.ps1
+++
@@ -1,4 +0,0 @@
-Remove-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Recurse -Confirm:$false -Force
-Write-Host Restarting explorer.exe ...
-$process = Get-Process -Name "explorer"
-Stop-Process -InputObject $process
