55a256f536954a9604a4ed7e3a285841fef98753
krrge <krrgegod@gmail.com>
2025-10-18 22:55:18 +0900

    Delete 右クリックの仕様変更 directory

--- 右クリックの仕様変更/RightClick classic.ps1
+++
@@ -1,4 +0,0 @@
-New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Name "InprocServer32" -force -value ""
-Write-Host Restarting explorer.exe ...
-$process = Get-Process -Name "explorer"
-Stop-Process -InputObject $process
